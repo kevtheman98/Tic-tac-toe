ce203fce5c4e4160ae109cba0705f1320db89bd7
Kevin <epickillerdude2007@gmail.com>
2024-02-03 22:43:15 -0500

    Linked array and cell click

--- script.js
+++ script.js
@@ -16,11 +16,6 @@ function gameBoard () {
     return(array)
 }
 
-
-
-
-
-
 function game() {
     const array = gameBoard();
     
@@ -43,6 +38,12 @@ function game() {
         } else if (array[0][2] != "" && array[0][2] == array [1][1] && array[0][2] == array[2][1]) {
             result = true
         }
+        /* For diagnoals*/
+        if(array[0][0] != "" && array[0][0] == array[1][1] && array[0][0] == array[2][2]) {
+            result = true
+        } else if (array[2][0] != "" && array[2][0] == array[1][1] && array[2][0] == array[0][2]) {
+            result = true
+        }
         return(result)
     }
 
@@ -53,33 +54,52 @@ function game() {
             for(let i = 0; i < element.length; i++) {
                 const cell = document.createElement('button')
                 cell.classList = "cell"
-                cell.id = "button" + num++
+                cell.id = num++
                 container.appendChild(cell)
-                cell.addEventListener("click", () => {
-                    if(player == 0) {
-                        cell.innerHTML = "X"
-                        player = 1
-                        
-                    } else {
-                        cell.innerHTML = "0"
-                        player = 0
-                        
-                    }
-                    
-                          
-                })
+                
                 
             }
         }
     }
 
-    
+    function marker() {
+        const cell = document.querySelectorAll(".cell")
+        console.log(cell)
+        function link(event) {
+            const buttonIndex = parseInt(event.target.id);
+            if (buttonIndex < 3) {
+                array[0][buttonIndex] = event.target.innerHTML;
+            } else if (3 <= buttonIndex && buttonIndex < 6) {
+                array[1][buttonIndex - 3] = event.target.innerHTML;
+            } else if (6 <= buttonIndex && buttonIndex <= 9) {
+                array[2][buttonIndex - 6] = event.target.innerHTML;
+            }
+            
+            console.log(array);
+        }
+        let currentPlayer = "X"
+        function switchPlayer() {
+            currentPlayer = (currentPlayer === "X") ? "O" : "X";
+        }
+        function changePlayer() {
+            this.innerHTML = currentPlayer;
+            switchPlayer();
+        }
+        for(let i = 0; i < cell.length; i++) {
+            cell[i].addEventListener("click", changePlayer) 
+            cell[i].addEventListener("click", link)
+        }
+        
+    }
 
-    return {checkWinner, render}
+    return {checkWinner, render, marker}
 }
 const childFunctions = game()
 
-childFunctions.checkWinner();
 childFunctions.render()
+childFunctions.marker()
+
+childFunctions.checkWinner();
+
 
 
